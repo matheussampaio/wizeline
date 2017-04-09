f5d4ddd53a0d182544697566710b92c762b542e8
Matheus Sampaio <matheus1401@gmail.com>
2017-04-09 16:59:35 -0300

    style: make it a little bit better

--- app/app.config.js
+++ app/app.config.js
@@ -1,4 +1,31 @@
-function WizeshortConfig($stateProvider, $urlRouterProvider, $locationProvider) {
+function WizeshortConfig($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
+    $mdThemingProvider.definePalette('wizeshortPalette', {
+        50: '#ffebee',
+        100: '#ffcdd2',
+        200: '#ef9a9a',
+        300: '#e57373',
+        400: '#ef5350',
+        500: '#262e30',
+        600: '#e53935',
+        700: '#d32f2f',
+        800: '#c62828',
+        900: '#b71c1c',
+        A100: '#ff8a80',
+        A200: '#ff5252',
+        A400: '#ff1744',
+        A700: '#d50000',
+
+        // whether, by default, text (contrast) on this palette should be dark or light
+        contrastDefaultColor: 'light',
+
+        //hues which contrast should be 'dark' by default
+        contrastDarkColors: ['50', '100', '200', '300', '400', 'A100'],
+        contrastLightColors: undefined    // could also specify this if default was 'dark'
+    });
+
+    $mdThemingProvider.theme('default')
+        .primaryPalette('wizeshortPalette');
+
 
     const appState = {
         name: 'app',
@@ -10,7 +37,7 @@ function WizeshortConfig($stateProvider, $urlRouterProvider, $locationProvider) 
         url: '/',
         name: 'shorten',
         parent: 'app',
-        template: '<shorten flex layout="column" layout-align="center center"></shorten>'
+        template: '<shorten layout flex></shorten>'
     };
 
     const allShortenState = {
